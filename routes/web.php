a56e76ceb507a2b4abc1ec7a1c96bd30318c6480
Zaid Kajee <zaid26@gmail.com>
2020-04-09 14:50:37 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::get('/', function () {
             $xmlData = simplexml_load_string(Http::get('https://sacoronavirus.co.za/feed/')->body());
 
             foreach ($xmlData->channel->item as $item) {
-                if (strpos(strtolower($item->title), 'update of covid-19') !== false) {
+                if (strpos(strtolower($item->title), 'update on covid-19') !== false) {
                     $info['title'] = (string) $item->title;
                     $info['link'] = (string) $item->link;
                     $info['data'] = (string) $item->children('http://purl.org/rss/1.0/modules/content/')->encoded;
